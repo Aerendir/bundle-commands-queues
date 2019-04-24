b864b82c1488ab5ad52a85a6f6d646f7500b6393
Aerendir <hello@aerendir.me>
2019-04-24 16:24:38 +0200

    Make possible to filter `Job`s by `status`.

--- src/Admin/Sonata/Entity/JobAdmin.php
+++ src/Admin/Sonata/Entity/JobAdmin.php
@@ -17,9 +17,13 @@ declare(strict_types=1);
 
 namespace SerendipityHQ\Bundle\CommandsQueuesBundle\Admin\Sonata\Entity;
 
+use SerendipityHQ\Bundle\CommandsQueuesBundle\Entity\Job;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\Type\Filter\ChoiceType;
 use Sonata\AdminBundle\Route\RouteCollection;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 /**
  * {@inheritdoc}
@@ -38,6 +42,26 @@ class JobAdmin extends AbstractAdmin
             ->addIdentifier('id')
             ->add('command')
             ->add('dependencies');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('status');
+
+        /*
+        $datagridMapper->add('status', 'doctrine_orm_choice',[], ChoiceType::class, [
+            'operator_type' => HiddenType::class,
+            'field_options' => [
+                'choices' => [
+                    'New' => Job::STATUS_NEW,
+                    'Aborted' => Job::STATUS_ABORTED,
+                ]
+            ],
+        ]);
+        */
     }
 
     /**
